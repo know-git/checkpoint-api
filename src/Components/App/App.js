151eb0c540f90889835f3e3f5d5db9568803bacb
unknown <ahmed.ajmi.pro@gmail.com>
2022-05-11 16:34:36 +0200

    api

---
+++ src/Components/App/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import UserList from "./../UserList/UserList.js";
+
+function App() {
+  const [listOfUSer, setListOfUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      axios(`https://jsonplaceholder.typicode.com/users`).then((result) => {
+        setListOfUser(result.data);
+        setIsLoading(false);
+      });
+    };
+    fetchItems();
+  }, []);
+
+  return (
+    <div className="App container">
+      <div className="row">
+        <UserList listOfUSer={listOfUSer} isLoading={isLoading} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
